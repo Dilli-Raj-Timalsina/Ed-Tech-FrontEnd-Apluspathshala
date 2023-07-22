61d88230d7c3b2094ea10846b0c1b39a4b813d29
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-07-22 18:57:23 +0545

    prettier setting added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-export default function Home(){
-return <div className="bg-slate-200 font-extrabold font-serif">
-  Hello Guys
-</div>
+export default function Home() {
+    return (
+        <div className=" bg-gradient-to-b from-purple-100 via-blue-50 to-green-50  h-screen w-screen ">
+            <div className="font-extrabold ">Hello Bro</div>
+        </div>
+    );
 }
