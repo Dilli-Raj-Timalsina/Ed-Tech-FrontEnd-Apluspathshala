594f844706b148ab7156f2574c830a98f1db41e2
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-07-24 06:45:37 +0545

    simple data fetching

--- app/upload/layout.tsx
+++ app/upload/layout.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+export const metadata: Metadata = {
+    title: "Creaty dilli",
+    description: "Generatee next app",
+};
+
 interface Props {
     children: React.ReactNode;
 }
@@ -6,6 +12,8 @@ export default function Layout({ children }: Props) {
     return (
         <div className="bg-red-600">
             <h1> Hey whatsapp , I am ffrom inner Layout</h1>
+            <a href="/upload/uploadInner">link</a>
+
             {children}
         </div>
     );
