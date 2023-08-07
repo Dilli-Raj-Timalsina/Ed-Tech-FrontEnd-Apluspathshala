59ab20826773786e43b41ae4747796d161b0e492
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-08-07 11:20:40 +0545

    add to cart button added in VideoBox

--- components/courseConsumeComponents/VideoBox.tsx
+++ components/courseConsumeComponents/VideoBox.tsx
@@ -1,12 +1,15 @@
 export default function VideoBox() {
     return (
-        <div className="mt-30 ml-96">
+        <div className="mt-30 ml-96 w-2/5  h-auto border-2 rounded-b-lg border-slate-200 flex flex-col justify-center">
             <video
-                width="320"
-                height="240"
+                className="w-full h-auto "
                 controls
+                autoPlay
                 src="https://youtu.be/NE0dWeV5epA"
             ></video>
+            <button className="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-5 py-2.5 mt-3 text-center ">
+                Add to cart
+            </button>
         </div>
     );
 }
