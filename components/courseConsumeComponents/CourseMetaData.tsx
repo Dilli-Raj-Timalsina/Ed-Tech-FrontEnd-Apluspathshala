c5e76ff69bf7031e84a71beb38637a231ee018a8
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-08-07 12:27:48 +0545

    CourseMetaData updated

--- components/courseConsumeComponents/CourseMetaData.tsx
+++ components/courseConsumeComponents/CourseMetaData.tsx
@@ -11,6 +11,93 @@ export default function CourseMetaData() {
                 Modern JavaScript from the beginning - all the way up to JS
                 expert level! THE must-have JavaScript resource in 2023.
             </div>
+
+            <div className="flex items-center m-3 mb-0 cursor-pointer">
+                <span className="text-yellow-600 text-base font-bold pr-1 inline-block">
+                    4.7
+                </span>
+                <svg
+                    className="w-4 h-4 text-yellow-500 mr-1"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+                <svg
+                    className="w-4 h-4 text-yellow-500 mr-1"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+                <svg
+                    className="w-4 h-4 text-yellow-500 mr-1"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+                <svg
+                    className="w-4 h-4 text-yellow-500 mr-1"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+                <svg
+                    className="w-4 h-4 text-slate-300 mr-1"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+
+                <a className="text-sm font-semibold text-blue-600 underline inline-block cursor-pointer">
+                    (52545 rating)
+                </a>
+
+                <p className="text-sm font-semibold ml-1 inline-block">
+                    181187 students
+                </p>
+            </div>
+            <div className="text-md font-semibold text-gray-800 drop-shadow-sm">
+                Created by :{" "}
+                <span className="text-md hover:drop-shadow-sm text-blue-600 cursor-pointer underline">
+                    Dilli Raj Timalsina
+                </span>
+                <div className="flex items-center">
+                    <div className="flex items-center">
+                        <img
+                            src="/updated-icon.svg"
+                            alt=""
+                            className="w-5 h-5"
+                        />
+                        <span className="text-sm font-semibold text-gray-700 m-1">
+                            Last updated 7/2023
+                        </span>
+                    </div>
+                    <div className="flex items-center ml-1">
+                        <img
+                            src="/language-icon.svg"
+                            alt=""
+                            className="w-5 h-5"
+                        />
+                        <span className="text-sm font-semibold text-gray-700 m-1">
+                            [English,hindi]
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
