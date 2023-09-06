ccb699712217e04c99503aa3a562aa4730b7690b
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-09-06 16:28:30 +0545

    forget password implemented

---
+++ app/forget-password/page.tsx
@@ -0,0 +1,81 @@
+"use client";
+import { useState, ChangeEvent } from "react";
+import { useRouter } from "next/navigation";
+import BounceSpinners from "@/components/spinners/BounceSpinner";
+
+export default function Home() {
+    const [email, setEmail] = useState<string>("");
+    const [message, setMessage] = useState<string>("");
+    const [sucess, setSucess] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+
+    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    };
+
+    const handleResetPassword = async () => {
+        setLoading(true);
+        const res = await fetch(
+            "http://localhost:3001/api/v1/user/forgetPassword",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email,
+                }),
+            }
+        );
+
+        if (res.ok) {
+            setMessage(`Password reset link sent to ${email}`);
+            setSucess(true);
+            setLoading(false);
+            router.push("/forget-password/verify-otp");
+        } else {
+            setMessage("user doesnot exist , please provide correct email");
+            setLoading(false);
+        }
+    };
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="max-w-md w-full px-4 py-8 bg-white shadow-lg rounded-lg">
+                <h2 className="text-2xl font-semibold text-gray-800 mb-6">
+                    Forget Password
+                </h2>
+
+                <div className="mb-4">
+                    <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="email"
+                    >
+                        Email
+                    </label>
+                    <input
+                        className="w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                        type="email"
+                        id="email"
+                        placeholder="Enter your email"
+                        value={email}
+                        onChange={handleEmailChange}
+                    />
+                </div>
+                <button
+                    className="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
+                    onClick={handleResetPassword}
+                    disabled={loading}
+                >
+                    {loading ? <BounceSpinners /> : "Reset Password"}
+                </button>
+                {message && sucess && (
+                    <p className="mt-4 text-sm text-green-500">{message}</p>
+                )}
+                {message && !sucess && (
+                    <p className="mt-4 text-sm text-red-500">{message}</p>
+                )}
+            </div>
+        </div>
+    );
+}
---
+++ app/forget-password/verify-otp/page.tsx
@@ -0,0 +1,124 @@
+"use client";
+import { useState, ChangeEvent } from "react";
+import { useRouter } from "next/navigation";
+import BounceSpinners from "@/components/spinners/BounceSpinner";
+
+function VerifyOTP() {
+    const [otp, setOtp] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [message, setMessage] = useState<string>("");
+    const [sucess, setSucess] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [password, setPassword] = useState<string>("");
+    const router = useRouter();
+
+    const handleOtpChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setOtp(e.target.value);
+    };
+    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    };
+    const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    const verifyOTP = async () => {
+        setLoading(true);
+        const res = await fetch(
+            "http://localhost:3001/api/v1/user/verifyToken",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email,
+                    otp,
+                    password,
+                }),
+            }
+        );
+
+        console.log(await res.json());
+
+        if (res.ok) {
+            setMessage(`Password Changed Succesfully`);
+            setSucess(true);
+            setLoading(false);
+            router.push("/login");
+        } else {
+            setMessage("OTP doeen't match !");
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="max-w-md w-full px-4 py-8 bg-white shadow-lg rounded-lg">
+                <h2 className="text-2xl font-semibold text-gray-800 mb-6">
+                    Verify OTP
+                </h2>
+                <div className="mb-4">
+                    <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="email"
+                    >
+                        Email
+                    </label>
+                    <input
+                        className="w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                        type="email"
+                        id="email"
+                        placeholder="Enter your email"
+                        value={email}
+                        onChange={handleEmailChange}
+                    />
+                    <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="password"
+                    >
+                        New Password
+                    </label>
+                    <input
+                        className="w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                        type="password"
+                        id="password"
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                    />
+
+                    <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="otp"
+                    >
+                        OTP
+                    </label>
+                    <input
+                        className="w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                        type="text"
+                        id="otp"
+                        placeholder="Enter OTP"
+                        value={otp}
+                        onChange={handleOtpChange}
+                    />
+                </div>
+                <button
+                    className="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
+                    onClick={verifyOTP}
+                    disabled={loading}
+                >
+                    {loading ? <BounceSpinners /> : "Verify OTP"}
+                </button>
+                {message && sucess && (
+                    <p className="mt-4 text-sm text-green-500">{message}</p>
+                )}
+                {message && !sucess && (
+                    <p className="mt-4 text-sm text-red-500">{message}</p>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default VerifyOTP;
